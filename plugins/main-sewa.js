25b3d6d7841d0af59346e1004d1d4b685630e816
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-13 08:35:17 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -3,41 +3,26 @@ let handler = async (m, { conn }) => {
     pepe = 'https://telegra.ph/file/0dfb35a7c7d0ada2e75d1.jpg'
     baper = await fetch(pepe).then(a => a.buffer())
     let listMessage = {
-        "title": "melcanz-bot",
+        "title": "nisacanz-bot",
         "description": `
-*melcanz-bot*
+*nisacanz-bot*
         
 1 Grup / 30 Hari
-Rp. 15,000 Dana, Ovo
-Rp. 20,000 Pulsa Axis/XL
+Rp. 15,000 Ovo
+Rp. 20,000 Pulsa Telkomsel
 
 1 Premium / 30 Hari
-Rp. 5,000 Dana, Ovo
-Rp. 10,000 Pulsa Axis/XL
+Rp. 5,000  Ovo
+Rp. 10,000 Pulsa Telkomsel
 
 tertarik? hubungi:
-@6287755080455 (amel)
-@6283128734012 (ariffb)
+@+6281387089252 (yusup)
+@+12096710019 (bot)
 `.trim(),
-        "listType": "PRODUCT_LIST",
-        "productListInfo": {
-            "productSections": [
-                {
-                    "title": "Klik untuk melihat harga",
-                    "products": [
-                        {
-                            "productId": "4696956640315324"
-                        }
-                    ]
-                }
-            ],
-            "headerImage": {
-                "productId": "4696956640315324",
-                "jpegThumbnail": baper
-            },
-            "businessOwnerJid": "6283128734012@s.whatsapp.net"
+        
+"businessOwnerJid": "12096710019@s.whatsapp.net"
         },
-        "footerText": "wa.me/6287755080455\nwa.me/6283128734012"
+        "footerText": "wa.me/6281387089252\nwa.me/12096710019"
     }
     conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
 }
